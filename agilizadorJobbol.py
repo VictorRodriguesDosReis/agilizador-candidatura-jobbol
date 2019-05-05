b24ebd25d1a3f65d1eeca8334851d5002f310152
VictorRodriguesDosReis <victor_reis005@hotmail.com>
2019-05-05 14:39:31 -0300

    Adicionado inferface de linha de comando:
    -Por ter implementado o módulo click para criar interface de linha de comando, foi necessário modificar a forma como o projeto inicia, por conta disso cada funcionalidade, como por exemplo o envio de e-mail, foi isolada em uma função que é responsável pelo comando.
    -Foi criado opções para os comandos de enviar e-mail e de configuração
    -As informações do e-mail agora são guardadas em um arquivo na pasta home do usuário em formato JSON.
    -A configuração das informações do e-mail são feitas através da linha de comando.

--- agilizadorJobbol.py
+++ agilizadorJobbol.py
@@ -1,30 +1,83 @@
-import sys
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import getpass
+import click
+from pathlib import Path
 from classes.email import Email
 from classes.webcrawler import WebCrawler
 
-fileURL = sys.argv[1]
-fileOpened = open(fileURL, 'r')
+@click.group(help="Agiliza candidatura as vagas de emprego do site Jobbol")
+def main():
+	createConfigurationFile()
 
-objectWebCrawler = WebCrawler()
+# Comando que envia os e-mails
+@main.command('enviar', help='Envia currículo utilizando e-mail')
+@click.option('urlFile', '--arquivo', '-a', help='Arquivo com as URLs das páginas de enviar candidatura')
+@click.option('pageUrl', '--url', '-u', help='URL da página de enviar candidatura')
+def commandSendEmail(urlFile, pageUrl):
+	HOME_DIRECTORY = str(Path.home())
+	configurationFile = Path(HOME_DIRECTORY+'/.agilJobbolConfig')
+	configurationJson = json.load(configurationFile.open())
+	objectWebCrawler = WebCrawler()
 
-fileAttachment = "seu-currículo.pdf"
-senderEmail = "seu_email@hotmail.com"
-emailPassword = "senha-do-seu-email"
-emailSubjectTemplate = "VAGA: {}"
-emailTextTemplate = (
-	"Prezado Recrutador,\n"
-	"Gostaria de me apresentar para a vaga de {} anunciada pela sua empresa.\n"
-	"Estou à disposição para maiores informações sobre meu perfil profissional.\n"
-	"Atenciosamente,\n"
-	"Seu Nome Completo.")
+	configurationJson['emailPassword'] = getpass.getpass('Senha do e-mail: ')
 
-for url in fileOpened:
+	if urlFile is not None:
+		urlFile = Path(urlFile)
+
+		for url in urlFile.open():
+			sendEmail(configurationJson, url)
+
+	if pageUrl is not None:
+		sendEmail(configurationJson, pageUrl)
+
+# Comando de configuração do e-mail
+@main.command('config', help="Configura as informações do e-mail")
+@click.option('email', '--email', '-e', help='E-mail a ser utilizado')
+@click.option('subject', '--assunto', '-a', help='Assunto do e-mail')
+@click.option('textBody', '--texto', '-t', help='Corpo de texto do e-mail')
+@click.option('pdf', '--pdf', '-p', help='Currículo em PDF')
+def commandConfiguration(email, subject, textBody, pdf):
+	HOME_DIRECTORY = str(Path.home())
+	configurationFile = Path(HOME_DIRECTORY+'/.agilJobbolConfig')
+	configurationJson = json.load(configurationFile.open())
+
+	if email is not None:
+		configurationJson['email'] = email
+
+	if subject is not None:
+		configurationJson['emailSubject'] = subject
+		
+	if textBody is not None:
+		if Path(textBody).is_file():
+			textFile = Path(textBody)
+			configurationJson['emailText'] = textFile.read_text()
+
+		else:
+			configurationJson['emailText'] = textBody
+		
+	if pdf is not None:
+		# Retorna o caminho absoluto do arquivo
+		absolutePath = Path(pdf)
+		configurationJson['attachmentFile'] = str(absolutePath.resolve())
+
+	configurationJson = json.dumps(configurationJson)
+	configurationFile.write_text(configurationJson)
+
+# Monta e envia o e-mail
+def sendEmail(configurationJson, pageUrl):
+	objectWebCrawler = WebCrawler()
+	objectWebCrawler.collectDatas(pageUrl)
+
+	emailSubjectTemplate = configurationJson['emailSubject']
+	emailTextTemplate = configurationJson['emailText']
+
 	objectEmailSender = Email(
-	fileAttachment = fileAttachment,
-	senderEmail = senderEmail,
-	emailPassword = emailPassword)
- 
-	objectWebCrawler.collectDatas(url)
+	attachmentFile = configurationJson['attachmentFile'],
+	senderEmail = configurationJson['email'],
+	emailPassword = configurationJson['emailPassword'])
 
 	emailSubject = emailSubjectTemplate.format(objectWebCrawler.getJobVacancy().upper())
 	emailBodyText = emailTextTemplate.format(objectWebCrawler.getJobVacancy().lower())
@@ -35,4 +88,25 @@ for url in fileOpened:
 
 	objectEmailSender.sendEmail()
 
-	print("{} - {}".format(objectWebCrawler.getJobVacancy(), objectWebCrawler.getEmployerEmail()))
+	click.echo("{} - {}".format(objectWebCrawler.getJobVacancy(), objectWebCrawler.getEmployerEmail()))
+
+# Cria o arquivo de configuração caso não exista
+def createConfigurationFile():
+	HOME_DIRECTORY = str(Path.home())
+	configurationFile = Path(HOME_DIRECTORY+'/.agilJobbolConfig')
+	configurationJson = None
+
+	# Verifica se o arquivo de configuração já foi criado
+	if not configurationFile.is_file():
+		configurationJson = {
+			'email': None,
+			'emailSubject': None,
+			'emailText': None,
+			'attachmentFile': None
+		}
+
+		configurationJson = json.dumps(configurationJson)
+		configurationFile.write_text(configurationJson)		
+
+if __name__ == "__main__":
+	main()
